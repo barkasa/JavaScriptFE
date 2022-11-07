1ca83cce8331791c7beadec905942575e1d85ca7
Ivan Morozov <107191857+barkasa@users.noreply.github.com>
2022-11-07 18:53:49 +0100

    Update script.js

--- 18/js/script.js
+++ 18/js/script.js
@@ -1,11 +1,3 @@
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-// let thirdAndFourthIndexes = arr.find((item) => {
-//   if (item > 3 && item < 4) {
-//     return true;
-//   }
-// });
-
-// console.log(thirdAndFourthIndexes); // 0
 
 const arr = ["Apple", "Orange", "Lemon", "Cherry", "Apple"];
 
